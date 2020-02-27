06ef0b4e5d257bcb6984130ec130a79fa79eb345
va.kozlov <vasiliy.kozlov@stoloto.ru>
2020-02-27 20:02:34 +0300

    add express

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const PORT = config.get('port') || 5001;
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+app.use('/api/admin/', require('./routes/routes.admin'));
+
+const start = async () => {
+  try {
+    // await mongoose.connect(config.get('mongoUri'), {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true
+    // });
+
+    app.listen(PORT, () => {
+      console.log(`Server run on port ${PORT}`);
+    });
+  } catch (e) {
+    res.status(500).json({ message: 'Что то пошло не так, попробуйте позже' });
+    process.exit(1);
+  }
+};
+
+start();
